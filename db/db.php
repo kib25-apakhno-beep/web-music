589e9ded7d73b0a934a6f9bd2b1e59d02fa141d5
kib25-apakhno-beep <kib25-a.pakhno@nubip.edu.ua>
2026-07-04 15:19:10 +0300

    каркас сайта

---
+++ db/db.php
@@ -0,0 +1,8 @@
+<?php
+$host = "localhost";   // IP бази даних
+$user = "root";        //логін для бази даних
+$pass = "";            // пароль для бази даних
+$dbname = "web-music"; // назва бази даних
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+?>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Music</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/footer.css">
+</head>
+<body>
+    <?php include 'php/header.php'; ?>
+
+    <div class="body">
+        <div class="content">
+            <h2>Welcome to Music</h2>
+            <p>This is the main content area.</p>
+        </div>
+    </div>
+
+    <?php include 'php/footer.php'; ?>
+</body>
+</html>
